9a0327004e3fa0af14ef799010876500ebd1209a
rohit <user@gmail.com>
2023-02-08 10:22:35 +0530

    API for user management svelte project

---
+++ Router/users.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const {deleteAll,displayUser,displaySingleUser,addUser,updateUser,deleteUser} = require("../Controller/usersController.js");
+const router = express.Router();
+
+
+//save student data into json file
+router.post("/user",addUser);
+  
+  //student data display
+router.get("/user",displayUser);
+
+  //get single student data 
+router.get('/user/:id', displaySingleUser)
+
+  // student data update
+router.put("/user/:id",updateUser );
+  
+  //deleting student
+router.delete("/user/:id",deleteUser );
+
+//deleting all elements from array
+router.delete("/user",deleteAll)
+
+module.exports = router  
